4b6e12097899d3d01771f84d19cb0bd50bd44505
Agata Filho <agata78@gmail.com>
2016-08-12 12:20:48 -0400

    Fix a bug. The `string` variable was missing `0`.

--- algorithms_DS_course/recursion/convert.py
+++ algorithms_DS_course/recursion/convert.py
@@ -2,12 +2,14 @@ from __future__ import division
 """Convert an integer to a string at any base."""
 
 def convert(n, base):
-    string = '123456789ABCDEF'
+    string = '0123456789ABCDEF'
     if n < base:
         return string[n]
     else:
-        return convert(n // 2, base) + string[n % base]
+        return convert(n // base, base) + string[n % base]
 
-if __name__ == '__nmae__':
+if __name__ == '__main__':
     assert convert(10, 2) == '1010'
+    assert convert(10, 16) == 'A'
+    assert convert(10, 16) != '1'
 
